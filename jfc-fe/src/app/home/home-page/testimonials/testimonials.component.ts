394808311a032e745b6ad7c0f6cd8e234b7a9895
Klerina <klerina.lamce@innovaway.al>
2023-10-21 16:42:12 +0200

    adding home

---
+++ jfc-fe/src/app/home/home-page/testimonials/testimonials.component.ts
@@ -0,0 +1,19 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as $ from 'jquery';
+import 'bootstrap';
+
+
+@Component({
+  selector: 'app-testimonials',
+  templateUrl: './testimonials.component.html',
+  styleUrls: ['./testimonials.component.scss']
+})
+export class TestimonialsComponent  implements AfterViewInit{
+  @ViewChild('testimonial-slider', { static: false }) myElementRef!: ElementRef;
+ 
+  ngAfterViewInit() {
+    //const nativeElement = this.myElementRef.nativeElement as HTMLElement;
+    console.log(this.myElementRef)
+   // nativeElement.style.display = 'none';
+  }
+}
--- jfc-fe/src/app/home/home.module.ts
+++ jfc-fe/src/app/home/home.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
+import { WhyChoseUsComponent } from './home-page/why-chose-us/why-chose-us.component';
+import { TestimonialsComponent } from './home-page/testimonials/testimonials.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    WhyChoseUsComponent,
+    TestimonialsComponent
   ],
   imports: [
     CommonModule
