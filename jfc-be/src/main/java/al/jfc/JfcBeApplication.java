a8aca1ad3ab93ffbb86c36e514d4b991687a9bf7
silvana.xhelili <silvana.xhelili@innovaway.al>
2023-10-21 09:43:42 +0200

    Adding starter be

---
+++ jfc-be/src/main/java/al/jfc/JfcBeApplication.java
@@ -0,0 +1,13 @@
+package al.jfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfcBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfcBeApplication.class, args);
+	}
+
+}
