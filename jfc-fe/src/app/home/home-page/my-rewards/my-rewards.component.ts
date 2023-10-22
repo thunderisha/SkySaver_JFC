a725c27d07e1d719579da71bd069c44d04974fdc
Regi Berisha <regi.berisha@innovaway.al>
2023-10-22 02:49:20 +0200

    Rewards update.

--- jfc-fe/src/app/home/home-page/my-rewards/my-rewards.component.ts
+++ jfc-fe/src/app/home/home-page/my-rewards/my-rewards.component.ts
@@ -1,52 +1,62 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component } from '@angular/core';
+import { ServicesService } from 'src/app/core/services.service';
 
 @Component({
   selector: 'app-my-rewards',
   templateUrl: './my-rewards.component.html',
   styleUrls: ['./my-rewards.component.scss']
 })
+
 export class MyRewardsComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource<MyRewards>(ELEMENT_DATA);
+  rewardsList = [
+      {
+        "idreward": 1,
+        "description": "5% Discount Hotel + Flight",
+        "level": {
+          "id": 1,
+          "description": "Rookie",
+          "endPoint": 20,
+          "startPoint": 1
+        }
+      },
+      {
+        "idreward": 2,
+        "description": "10% Discount + Priority Check In",
+        "level": {
+          "id": 2,
+          "description": "Expert",
+          "endPoint": 40,
+          "startPoint": 20
+        }
+      },
+      {
+        "idreward": 3,
+        "description": "10% Discount + Lounge Access ",
+        "level": {
+          "id": 3,
+          "description": "Master",
+          "endPoint": 60,
+          "startPoint": 40
+        }
+      },
+      {
+        "idreward": 4,
+        "description": "20% Discount + Free Seat Upgrade",
+        "level": {
+          "id": 4,
+          "description": "Super Master",
+          "endPoint": 80,
+          "startPoint": 60
+        }
+      }
+    ];
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private servicesService: ServicesService) { }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit() {
+
   }
+
 }
 
-export interface MyRewards {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: MyRewards[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-];
